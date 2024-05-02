f856ba2e6ba7e7b3402c7f3fa63f92e212b5a873
sshnnyklhh <shenynurkholifah.com>
2024-05-02 19:47:07 +0700

    230441100048_ShenyNurKholifah_Modul3_KakTamam

---
+++ modul-3/230441100048_ShenyNurKholifah/Tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./tailwind.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#14b8a6',
+      }
+    },
+  },
+  plugins: [],
+}
+
