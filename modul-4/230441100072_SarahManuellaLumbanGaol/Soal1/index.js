4457130b78892ba03e54a16200bbf4e7b707212b
fikrihasanulnafis <fikrihasanul566@gmail.com>
2024-06-23 09:35:28 +0700

    23044100095-FikriHasanulNafis-Modul4-AsprakKakArdi

--- modul-4/230441100072_SarahManuellaLumbanGaol/Soal1/index.js
+++
@@ -1,9 +0,0 @@
-// document.getElementsByClassName("dropdown-btn")[0].addEventListener("click", function() {
-//     this.classList.toggle("responsive");
-//     document.getElementsByClassName("dropdown-content")[0].classList.toggle("responsive");
-//   });
-
-let subMenu = document.getElementById("subMenu");
-function toggleMenu() {
-  subMenu.classList.toggle("open-menu");
-}
